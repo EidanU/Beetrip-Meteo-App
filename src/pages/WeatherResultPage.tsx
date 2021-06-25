fef73e48a4550566452cefe962b75a8269a79a8e
EidanU <71008360+EidanU@users.noreply.github.com>
2021-06-25 16:56:13 +0200

    Add : WeatherResultPage / MeteoDisplay + css

--- src/pages/WeatherResultPage.tsx
+++ src/pages/WeatherResultPage.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from 'react';
+import getApi from '../commons/services';
+import MeteoDisplay from '../components/MeteoDisplay';
+import '../styles/weatherResultPage.css'
 
-const HomePage: React.FC = ()=> {
-    return(
+
+const WeatherResultPage: React.FC = ()=> {
+
+    const pathArray: string[] = window.location.pathname.split('/');
+    const lastUrlItem: string = pathArray[pathArray.length -1];
+    const [item, setstate] = useState<string>(lastUrlItem);
+    const [weather, setWeather] = useState<any>({});
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+         getApi(item).then((weather)=>{
+             setWeather(weather)
+             setLoading(false)
+            });
+    },[])
+
+    if(loading) return <p>La data charge</p>;
+    else{ 
+        return (
         <>
-       <h1>Weather results page</h1>
+            { weather.data.main.temp - 273.15 < 15
+                ? 
+                <MeteoDisplay bgColor={'cold'} text={'froid'}/>
+            :
+            <MeteoDisplay bgColor={'hot'} text={'chaud'}/>
+        }
         </>
-    );
+    )
+
+         
+    }
+
 };
-export default HomePage;
+export default WeatherResultPage;
+
